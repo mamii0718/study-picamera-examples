abf1c4fd8227f330b97829b312b255531d5b4abd
mamii0718 <mamii07180@gmail.com>
2019-01-10 20:52:24 +0900

    Open CVと文字表示

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -43,3 +43,5 @@ class FaceDetector(object):
 
         # frameを戻り値として返す
         return frame
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
